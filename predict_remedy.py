dd6fb18b70f4d016328bb3342aab045c00b79951
fatimakhan9200 <fatimakhan91080@gmail.com>
2026-05-26 01:39:46 +0500

    Update predict_remedy.py

--- predict_remedy.py
+++ predict_remedy.py
@@ -1,4 +1,4 @@
-# predict_remedy.py
+
 
 import numpy as np
 from PIL import Image
@@ -6,30 +6,20 @@ import tensorflow as tf
 import json
 import os
 
-# -----------------------------
-# 1️⃣ Load model
-# -----------------------------
+
 model_path = os.path.join("model", "crop_disease_model1.h5")
 
 model = tf.keras.models.load_model(model_path)
 
-# -----------------------------
-# 2️⃣ Load class names
-# -----------------------------
 class_names_path = os.path.join("model", "class_names.json")
 with open(class_names_path, "r") as f:
     class_names = json.load(f)
 
-# -----------------------------
-# 3️⃣ Load remedies
-# -----------------------------
+
 remedies_path = "remedies.json"
 with open(remedies_path, "r") as f:
     remedies_dict = json.load(f)
 
-# -----------------------------
-# 4️⃣ Prediction function (TOP 3)
-# -----------------------------
 def predict_and_remedy(img_path):
 
     # Read image (NO cv2 → Streamlit safe fix)
